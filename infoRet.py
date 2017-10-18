80cd002bf182efaa5aae9a6862f19639d2990e7c
Eddie <cueto303@gmail.com>
2017-10-18 17:00:57 -0500

    changes

--- infoRet.py
+++ infoRet.py
@@ -1,7 +1,22 @@
 import feedparser as fp
+#  from time import gmtime, strftime
 
-datUniver = fp.parse('http://www.eluniversal.com.mx/seccion/1/rss.xml')
-datJorn = fp.parse('http://www.jornada.unam.mx/rss/politica.xml?v=1')
+
+def get_data_rss():
+
+    datUniver = fp.parse('http://www.eluniversal.com.mx/seccion/1/rss.xml')
+    datJorn = fp.parse('http://www.jornada.unam.mx/rss/politica.xml?v=1')
+
+    file = open('rss_univ.txt', 'a')
+
+    file.write(str(datUniver.headers['Date']) + ';\n')
+    file.write(str(datUniver) + ';\n')
+    file.write(str(datJorn.headers['Date']) + ';\n')
+    file.write(str(datJorn) + ';\n')
+
+    file.close()
+
+#  SOME COMMANDS OF FEEDPARSER
 
 #  print(datUniver['feed']['link'] + '\n')
 #  print(datJorn['feed']['title'] + '\n')
@@ -13,11 +28,5 @@ datJorn = fp.parse('http://www.jornada.unam.mx/rss/politica.xml?v=1')
 
 #  print(datUniver)
 
-file = open('rss_univ.txt', 'a')
 
-file.write(str(datUniver.headers['Date']) + ';\n')
-file.write(str(datUniver) + ';\n')
-file.write(str(datJorn.headers['Date']) + ';\n')
-file.write(str(datJorn) + ';\n')
-
-file.close()
+#print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
