112c3b2517bb2bba7e108167ff3ffd3353924ceb
Eddie <cueto303@gmail.com>
2017-10-19 11:03:48 -0500

    Created a thread for get_data_rss()

--- infoRet.py
+++ infoRet.py
@@ -6,15 +6,20 @@ def get_data_rss():
 
     datUniver = fp.parse('http://www.eluniversal.com.mx/seccion/1/rss.xml')
     datJorn = fp.parse('http://www.jornada.unam.mx/rss/politica.xml?v=1')
+    datCnn = fp.parse('http://expansion.mx/rss/politica')
 
     file = open('rss_univ.txt', 'a')
 
+    file.write(str(datCnn['Date']) + ';\n')
+    file.write(str(datCnn) + ';\n')
     file.write(str(datUniver.headers['Date']) + ';\n')
     file.write(str(datUniver) + ';\n')
     file.write(str(datJorn.headers['Date']) + ';\n')
     file.write(str(datJorn) + ';\n')
 
     file.close()
+
+get_data_rss()
 
 #  SOME COMMANDS OF FEEDPARSER
 
