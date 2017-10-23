824624df3a2ecc719336ec3ae665bd474e9ead89
Eddie <cueto303@gmail.com>
2017-10-23 16:55:37 -0500

    infBack changes to portrait how to extract important data from the dictionaries

--- infBack.py
+++ infBack.py
@@ -1,11 +1,18 @@
 import yaml
+import feedparser as fp
 
 rawDat = open('rss_univ.txt', 'r')
 
 strDat = rawDat.read()
 
 rawDat = strDat.split(';\n')
+index = len(rawDat) - 1
+rawDat.pop(index)
 
-# rawDat = yaml.load(rawDat[0])
+strDat = yaml.load(rawDat[0])
 
-print(rawDat[0])
+# this section of the code show how to extract relevant data from the dictionaries
+print(len(rawDat))
+print(strDat['entries'][0]['title'])
+print(strDat['entries'][0]['links'][0]['href'])
+print(strDat['entries'][0]['summary'])
